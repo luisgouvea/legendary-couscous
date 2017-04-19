22982a99afc37ecca4e8823555ba239a404ac212
Luis Eduardo <luiseduardo.gouvea@gmail.com>
2017-04-18 21:52:30 -0300

    Create EsquemaDB.sql

---
+++ EsquemaDB.sql
@@ -0,0 +1,8 @@
+CREATE TABLE produtos  
+( id_produtos(10) NOT NULL,  
+  genero varchar2(200) NOT NULL,  
+  desconto number(15),
+  nomeAutor varchar2(200),
+  titulo varchar2(200),
+  CONSTRAINT id_produtos PRIMARY KEY (id_produtos)  
+);  
